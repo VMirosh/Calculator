af96d172725743fc6245db7ce2177578a8857f83
Vladyslav Miroshnychenko <miroshnychenko.vlad@gmail.com>
2023-01-20 14:14:32 +0100

    create new file and style

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let showWrite = document.querySelector(".show-write");
 let result = document.querySelector(".result");
 
 let buttons = document.querySelector(".buttons");
-
 buttons.addEventListener("click", listener);
 
 function listener(event){
+
 console.log(event.target);
 }
 

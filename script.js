1831d5bfaec172ebf4145dd1f666c767e35aa605
Vladyslav Miroshnychenko <miroshnychenko.vlad@gmail.com>
2023-01-20 01:41:06 +0100

    instale new style

---
+++ script.js
@@ -0,0 +1,14 @@
+let outputResult = document.querySelector(".scren");
+let showWrite = document.querySelector(".show-write");
+let result = document.querySelector(".result");
+
+let buttons = document.querySelector(".buttons");
+
+buttons.addEventListener("click", listener);
+
+function listener(event){
+console.log(event.target);
+}
+
+
+

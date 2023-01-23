e491a46fd049adf49002e93df7311508081fac3b
Vladyslav Miroshnychenko <miroshnychenko.vlad@gmail.com>
2023-01-23 16:30:34 +0100

    delete unuse code

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 let outputResult = document.querySelector(".scren");
-// let showWrite = document.querySelector(".show-write");
-// let resultShower = document.querySelector(".result");
 let buttons = document.querySelector(".buttons");
+
 buttons.addEventListener("click", listener);
+document.addEventListener("keydown", function (event) {
+
+  switch (event.key) {
+    case 'Enter':
+      listener;
+      break;
+  }
+})
+
 let result;
 let operation;
 let second;
 let first;
 let inputData = [];
-
+//Обьект в котором записаны функции для операций
 let operations = {
   '+': (first, second) => { return first + second; },
   '-': (first, second) => { return first - second; },
@@ -16,72 +24,63 @@ let operations = {
   '÷': (first, second) => { return first / second; },
 }
 
-
 function listener(event) {
+
   let btnContent = event.target.textContent;
 
-  if (btnContent == '=') {
-    calculate();
-    inputData = [];
-    return;
-  }
-  
-  if (btnContent == 'C') {
-    inputData = [];
-    // let string = inputData.join('');
-    // outputResult.append(string);
-    return;
-  }
-
   if (event.target.nodeName == 'TD') {
-    let lastElement = inputData[inputData.length - 1];
+    if (btnContent == '=') {
+      calculate();
+      inputData = [];
+      console.log(inputData);
+      return;
+    }
+    if (btnContent == 'C') {
+      inputData = [];
+      outputResult.textContent = '0';
+      console.log(inputData);
+      return;
+    }
 
-    if ((!isNaN(Number(lastElement))) && ((!isNaN(Number(btnContent))) || (btnContent == '.'))) {
-      inputData[inputData.length - 1] = lastElement + btnContent;
+    let lengthData = inputData.length;
+    let lastElementData = inputData[lengthData - 1];
 
+    if ((!isNaN(Number(lastElementData))) && ((!isNaN(Number(btnContent))) || (btnContent == '.'))) {
+      inputData[lengthData - 1] = lastElementData + btnContent;
     } else {
       inputData.push(btnContent);
     }
 
-    console.log(inputData);
-  }
-  if (btnContent == '←') {
-    inputData.pop(btnContent);
-    inputData.pop(btnContent);
+    if (btnContent == '←') {
+      if ((lengthData == 2) || (lengthData == 0) || (lengthData == 1)) {
+        inputData.pop(btnContent);
+        inputData = [];
+        outputResult.textContent = `0`;
+        console.log(inputData);
+        return;
+      } else {
+        inputData.pop(btnContent);
+        inputData.pop(btnContent);
+      }
+    }
+    let string = inputData.join('');
+    outputResult.textContent = `${string}`;
     console.log(inputData);
   }
 }
-function calculateEval() {
-  let string = inputData.join('');
-  console.log(string);
-  result = eval(string);
-  console.log(result);
-}
+
 function calculate() {
   result = Number(inputData[0]);
-
   for (let i = 0; i < inputData.length - 1; i += 2) {
     second = Number(inputData[i + 2]);
     operation = inputData[i + 1];
     result = operations[operation](result, second);
-    // switch (operation) {
-    //   case '+':
-    //     result = result + second;
-    //     break;
-    //   case '-':
-    //     result = result - second;
-    //     break;
-    //   case '×':
-    //     result = result * second;
-    //     break;
-    //   case '÷':
-    //     result = result / second;
-    //     break;
-    //   }
   }
   outputResult.textContent = result.toFixed(2);
   console.log(result.toFixed(2));
 }
+
+
 
 
 
